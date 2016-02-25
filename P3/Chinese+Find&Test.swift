896f614ef86bb9bd8d8626c58ec83e521ca2c116
Yao Xiao <xiaoyao901010@gmail.com>
2016-02-25 16:59:24 +0800

    done with pinyin! finally

---
+++ P3/Chinese+Find&Test.swift
@@ -0,0 +1,83 @@
+//
+//  Chinese+Find&Test.swift
+//  P3
+//
+//  Created by 文川术 on 2/24/16.
+//  Copyright © 2016 myname. All rights reserved.
+//
+
+import Foundation
+
+
+extension Chinese {
+
+//	func pinyinFromCharacter(character: String) {
+//		let str = NSMutableString(string: character) as CFMutableStringRef
+//		CFStringTransform(str, nil, kCFStringTransformMandarinLatin, false)
+//		CFStringTransform(str, nil, kCFStringTransformStripDiacritics, false)
+//		print(str)
+//	}
+//
+//	func getAllPinyinsHaveTwoCharacters() {
+//		var result = [String]()
+//
+//		for pinyin in allPossiblePinyin {
+//			let characters = charactersFromPinyin(pinyin)
+//			if characters.count >= 2 {
+//				result.append(pinyin)
+//			}
+//		}
+//
+//		print(result)
+//	}
+//
+//	func confusablePinyin_Different_Test() {
+//		for couple in confusablePinyin_Different {
+//			let pinyins = couple.componentsSeparatedByString(" ")
+//
+//			let words_0 = charactersFromPinyin(pinyins[0])
+//			if words_0.count == 0 { print(pinyins[0]) }
+//
+//			let words_1 = charactersFromPinyin(pinyins[1])
+//			if words_1.count == 0 { print(pinyins[1]) }
+//
+//			if pinyins.count != 2 { print(pinyins) }
+//		}
+//	}
+//
+//	func confusablePinyinOfWord_Different_Test() {
+//		for couple in confusablePinyinOfWord_Different {
+//			let wordPinyins = couple.componentsSeparatedByString("-")
+//
+//			let words_0 = wordsFromPinyin(wordPinyins[0])
+//			if words_0.count == 0 { print(wordPinyins[0]) }
+//
+//			let words_1 = wordsFromPinyin(wordPinyins[1])
+//			if words_1.count == 0 { print(wordPinyins[1]) }
+//
+//			if wordPinyins.count != 2 { print(wordPinyins) }
+//		}
+//	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
